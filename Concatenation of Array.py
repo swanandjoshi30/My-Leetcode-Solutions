197a81ebcbe6b19b8d485443ac47a2893eef46fd
Swanand Arvind Joshi <96830440+swanandjoshi30@users.noreply.github.com>
2024-03-16 21:21:41 +0530

    Concatenation of Array.py
    Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
    
    Specifically, ans is the concatenation of two nums arrays.
    
    Return the array ans.

---
+++ Concatenation of Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        
+        # Concatenate the original list 'nums' with the copy 'num2'
+        # and return the result
+        num2=nums
+        return nums+num2
