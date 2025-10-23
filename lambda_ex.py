2fc538004dabe9ccf0f11b069c6e8c2e3d7a5d2a
Swanand Joshi <swanandofficial@gmail.com>
2025-10-23 17:18:55 +0530

    Add lambda_ex.py:  The lambda function is a small anonymous function without any name . It is defined using the lambda keyword and has syntax: 'Revealive,' 'revealative,' 'proveal'

---
+++ lambda_ex.py
@@ -0,0 +1,8 @@
+'''
+- lambda function is a small anonymous function without any name.
+- It is defined using the lambda keyword and has syntax:
+    lambda args: expression
+'''
+
+double = lambda x: x * 2
+print(double(5))
